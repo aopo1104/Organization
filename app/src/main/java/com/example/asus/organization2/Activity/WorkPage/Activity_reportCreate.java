9d8d5662a289138a110a2163d004480f1ab3dd17
aopo1104 <rrrr_517036283@qq.com>
2020-07-11 17:14:48 +0800

    test

---
+++ app/src/main/java/com/example/asus/organization2/Activity/WorkPage/Activity_reportCreate.java
@@ -0,0 +1,21 @@
+package com.example.asus.organization2.Activity.WorkPage;
+
+import android.os.Bundle;
+
+import com.example.asus.organization2.Activity.Activity_Base;
+import com.example.asus.organization2.R;
+
+/**
+ * Created by ASUS on 2019/4/15.
+ */
+
+public class Activity_reportCreate extends Activity_Base {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_create);
+
+        setTitle("每日汇报");
+    }
+
+}
